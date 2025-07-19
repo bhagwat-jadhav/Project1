3a01802d4f928c78a203cd9904e086d4df8cbbb8
bhagwat-jadhav <bhagwatjadhav649@gmail.com>
2025-07-19 10:05:33 +0530

    Post server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const PORT= process.env.PORT || 3000;
 app.get('/login',(req,res)=>{
         res.sendFile(path.join(__dirname,'Index.html')); 
         });
+app.post('/login',(req,res)={
+        res.send(req.body);
+}
                  app.listen(PORT,()=>{
                             console.log('server running at port 3000');
                             });
