6d0dd0f88edc5c750098034f8070651431319979
bhagwat-jadhav <bhagwatjadhav649@gmail.com>
2025-07-19 09:50:17 +0530

    File added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path= require("path");
 const app = express ();
 const PORT= process.env.PORT || 3000;
 app.get('/login',(req,res)=>{
-        res.send('hii2'); 
+        res.sendFile(path.join(__dirname,'index.html')); 
         });
                  app.listen(PORT,()=>{
                             console.log('server running at port 3000');
